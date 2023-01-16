416f2a47b2806c17104ac112a1f71ba53898385e
RuyHa <code.ruyha@gmail.com>
2023-01-16 14:05:01 +0900

    [Feat] nav 추가 및 테이블뷰 생성

--- FlowRuyHaTestApp/FlowRuyHaTestApp/MainViewController.swift
+++ FlowRuyHaTestApp/FlowRuyHaTestApp/MainViewController.swift
@@ -7,14 +7,49 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+import SnapKit
 
+class MainViewController: UIViewController {
+    
+    var testArray = [1,2]
+    
+    private lazy var myTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .red
-        // Do any additional setup after loading the view.
+        self.title = "앨범"
+        setLayout()
     }
-
-
+    
 }
 
+extension MainViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return testArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let myCell = UITableViewCell()
+        myCell.textLabel?.text = "\(testArray[indexPath.row])"
+        return myCell
+    }
+    
+}
+
+
+//MARK: 오토레이아웃관련 코드
+extension MainViewController {
+    
+    func setLayout(){
+        view.addSubview(myTableView)
+        myTableView.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview()
+            $0.top.bottom.equalToSuperview()
+        }
+    }
+}
