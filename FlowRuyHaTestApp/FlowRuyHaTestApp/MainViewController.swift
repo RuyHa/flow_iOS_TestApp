5306b1a81a05f361cf82d72f0dfff01d1dc8eb03
RuyHa <code.ruyha@gmail.com>
2023-01-16 11:41:30 +0900

    [feat] 스토리보드 제거 및 snapKit 추가

---
+++ FlowRuyHaTestApp/FlowRuyHaTestApp/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlowRuyHaTestApp
+//
+//  Created by Ruyha on 2023/01/16.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
